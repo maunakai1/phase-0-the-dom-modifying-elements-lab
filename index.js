f2d29de658598313e54fa89d136f9bb3b1626a29
Kai Del Rosario <Maunakai123@yahoo.com>
2023-07-13 19:50:34 -0700

    hello

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR_NAME is the champion";
+document.body.append(newHeader);
